899d2ca332c75b628ef1429ecbf465dbb4f55a0e
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-02-05 19:41:54 +0200

    fixed getting module connection string

--- VirtoCommerce.CartModule.Web/Module.cs
+++ VirtoCommerce.CartModule.Web/Module.cs
@@ -15,7 +15,7 @@ namespace VirtoCommerce.CartModule.Web
 {
     public class Module : ModuleBase
     {
-        private readonly string _connectionStringName = ConfigurationHelper.GetConnectionStringValue("{{ModuleId}}") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
+        private readonly string _connectionStringName = ConfigurationHelper.GetConnectionStringValue("VirtoCommerce.Cart") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
         private readonly IUnityContainer _container; 
 
         public Module(IUnityContainer container)

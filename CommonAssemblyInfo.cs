6605fa04b1a5d2387308ca7d48d0d9b9a101b104
Eugeny Tatarincev <et@virtoway.com>
2018-06-06 12:29:20 +0200

    2.16.10

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // <copyright file="GlobalAssemblyInfo.cs" company="VirtoCommerce">
 //   Copyright © VirtoCommerce. All rights reserved.
 // </copyright>
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Cart Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2017")]
 
-[assembly: AssemblyFileVersion("2.16.9.0")]
-[assembly: AssemblyVersion("2.16.9.0")]
+[assembly: AssemblyFileVersion("2.16.10.0")]
+[assembly: AssemblyVersion("2.16.10.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

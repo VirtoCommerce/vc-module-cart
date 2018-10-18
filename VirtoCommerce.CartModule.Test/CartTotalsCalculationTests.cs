a0edda5f83e977579e111c5088ae0f6fe99447a2
Ilya <ilya.ivanov6@gmail.com>
2018-10-18 19:56:43 +0200

    Added new test

--- VirtoCommerce.CartModule.Test/CartTotalsCalculationTests.cs
+++ VirtoCommerce.CartModule.Test/CartTotalsCalculationTests.cs
@@ -28,6 +28,44 @@ namespace VirtoCommerce.CartModule.Test
             Assert.Equal(44.95m, cart.Total);
         }
 
+        [Fact]
+        public void CalculateTotals_Should_Be_RightTotals2()
+        {
+            var item1 = new LineItem { ListPrice = 10.20m, SalePrice = 9.12m, DiscountAmount = 1.33m, TaxPercentRate = 0.12m, Fee = 0.12m, Quantity = 2 };
+            var payment = new Payment { Price = 44.52m, DiscountAmount = 10, TaxPercentRate = 0.12m };
+            var shipment = new Shipment { Price = 20.0m, DiscountAmount = 4m, TaxPercentRate = 0.12m };
+
+            var cart = new ShoppingCart
+            {
+                TaxPercentRate = 0.12m,
+                Fee = 13.11m,
+                Items = new List<LineItem> { item1 },
+                Payments = new List<Payment> { payment },
+                Shipments = new List<Shipment> { shipment }
+            };
+
+            var totalsCalculator = new DefaultShopingCartTotalsCalculator();
+            totalsCalculator.CalculateTotals(cart);
+
+            Assert.Equal(11.424m, item1.ListPriceWithTax);
+            Assert.Equal(10.2144m, item1.SalePriceWithTax);
+            Assert.Equal(9.12m, item1.PlacedPrice);
+            Assert.Equal(18.24m, item1.ExtendedPrice);
+            Assert.Equal(1.4896m, item1.DiscountAmountWithTax);
+            Assert.Equal(2.66m, item1.DiscountTotal);
+            Assert.Equal(0.1344m, item1.FeeWithTax);
+            Assert.Equal(10.2144m, item1.PlacedPriceWithTax);
+            Assert.Equal(20.4288m, item1.ExtendedPriceWithTax);
+            Assert.Equal(2.9792m, item1.DiscountTotalWithTax);
+            Assert.Equal(2.2032m, item1.TaxTotal);
+
+            Assert.Equal(4.48m, shipment.DiscountAmountWithTax);
+            Assert.Equal(22.4m, shipment.PriceWithTax);
+            Assert.Equal(0.0m, shipment.FeeWithTax);
+            Assert.Equal(16.0m, shipment.Total);
+            Assert.Equal(17.92m, shipment.TotalWithTax);
+            Assert.Equal(1.92m, shipment.TaxTotal);
+        }
 
         [Fact]
         public void CalculateTotals_Should_Be_RightTotals()

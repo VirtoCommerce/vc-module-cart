9d0def870c95c5b38043e89393b1b1be92c9d113
Ilya <ilya.ivanov6@gmail.com>
2018-10-24 16:43:15 +0200

    Shipping totals are checked

--- VirtoCommerce.CartModule.Test/CartTotalsCalculationTests.cs
+++ VirtoCommerce.CartModule.Test/CartTotalsCalculationTests.cs
@@ -15,58 +15,19 @@ namespace VirtoCommerce.CartModule.Test
         public void CalculateTotals_CartTotals_MustBe_Sum_Of_Parts_After_Round()
         {
             var item1 = new LineItem { ListPrice = 49.95m, SalePrice = 49.95m, DiscountAmount = 4.995m, TaxPercentRate = 0m, Fee = 0m, Quantity = 1 };
-            var item2 = new LineItem { ListPrice = 13.43m, SalePrice = 13.43m, DiscountAmount = 4.995m, TaxPercentRate = 0m, Fee = 0m, Quantity = 1 };
 
             var cart = new ShoppingCart
             {
-                Items = new List<LineItem> { item1,item2 },
-            };
-            var totalsCalculator = new DefaultShopingCartTotalsCalculator();
-            totalsCalculator.CalculateTotals(cart);
-
-            Assert.Equal(63.38m, cart.SubTotal);
-            Assert.Equal(9.99m, cart.DiscountTotal);
-            Assert.Equal(53.39m, cart.Total);
-        }
-
-        [Fact]
-        public void CalculateTotals_Should_Be_RightTotals2()
-        {
-            var item1 = new LineItem { ListPrice = 10.20m, SalePrice = 8.87m, DiscountAmount = 1.33m, TaxPercentRate = 0.12m, Fee = 0.12m, Quantity = 2 };
-            var payment = new Payment { Price = 44.52m, DiscountAmount = 10, TaxPercentRate = 0.12m };
-            var shipment = new Shipment { Price = 20.0m, DiscountAmount = 4m, TaxPercentRate = 0.12m };
-
-            var cart = new ShoppingCart
-            {
-                TaxPercentRate = 0.12m,
-                Fee = 13.11m,
                 Items = new List<LineItem> { item1 },
-                Payments = new List<Payment> { payment },
-                Shipments = new List<Shipment> { shipment }
             };
-
             var totalsCalculator = new DefaultShopingCartTotalsCalculator();
             totalsCalculator.CalculateTotals(cart);
 
-            Assert.Equal(11.424m, item1.ListPriceWithTax);
-            Assert.Equal(9.9344m, item1.SalePriceWithTax);
-            Assert.Equal(8.87m, item1.PlacedPrice);
-            Assert.Equal(17.74m, item1.ExtendedPrice);
-            Assert.Equal(1.4896m, item1.DiscountAmountWithTax);
-            Assert.Equal(2.66m, item1.DiscountTotal);
-            Assert.Equal(0.1344m, item1.FeeWithTax);
-            Assert.Equal(9.9344m, item1.PlacedPriceWithTax);
-            Assert.Equal(19.8688m, item1.ExtendedPriceWithTax);
-            Assert.Equal(2.9792m, item1.DiscountTotalWithTax);
-            Assert.Equal(2.1432m, item1.TaxTotal);
-
-            Assert.Equal(4.48m, shipment.DiscountAmountWithTax);
-            Assert.Equal(22.4m, shipment.PriceWithTax);
-            Assert.Equal(0.0m, shipment.FeeWithTax);
-            Assert.Equal(16.0m, shipment.Total);
-            Assert.Equal(17.92m, shipment.TotalWithTax);
-            Assert.Equal(1.92m, shipment.TaxTotal);
+            Assert.Equal(49.95m, cart.SubTotal);
+            Assert.Equal(5m, cart.DiscountTotal);
+            Assert.Equal(44.95m, cart.Total);
         }
+
 
         [Fact]
         public void CalculateTotals_Should_Be_RightTotals()
@@ -75,7 +36,6 @@ namespace VirtoCommerce.CartModule.Test
             var item1 = new LineItem { ListPrice = 10.99m, SalePrice = 9.66m, DiscountAmount = 1.33m, TaxPercentRate = 0.12m, Fee = 0.33m, Quantity = 2 };
             var item2 = new LineItem { ListPrice = 55.22m, SalePrice = 49.33m, DiscountAmount = 5.89m, TaxPercentRate = 0.12m, Fee = 0.12m, Quantity = 5 };
             var item3 = new LineItem { ListPrice = 88.45m, SalePrice = 77.67m, DiscountAmount = 10.78m, TaxPercentRate = 0.12m, Fee = 0.08m, Quantity = 12 };
-            var item4 = new LineItem { ListPrice = 34.42m, SalePrice = 26.54m, DiscountAmount = 7.88m, TaxPercentRate = 0.12m, Fee = 0.10m, Quantity = 2 };
             var payment = new Payment { Price = 44.52m, DiscountAmount = 10, TaxPercentRate = 0.12m };
             var shipment = new Shipment { Price = 22.0m, DiscountAmount = 5m, TaxPercentRate = 0.12m };
 
@@ -83,7 +43,7 @@ namespace VirtoCommerce.CartModule.Test
             {
                 TaxPercentRate = 0.12m,
                 Fee = 13.11m,
-                Items = new List<LineItem> { item1, item2, item3, item4 },
+                Items = new List<LineItem> { item1, item2, item3 },
                 Payments = new List<Payment> { payment },
                 Shipments = new List<Shipment> { shipment }
             };
@@ -102,18 +62,6 @@ namespace VirtoCommerce.CartModule.Test
             Assert.Equal(2.9792m, item1.DiscountTotalWithTax);
             Assert.Equal(2.358m, item1.TaxTotal);
 
-            Assert.Equal(38.5504m, item4.ListPriceWithTax);
-            Assert.Equal(29.7248m, item4.SalePriceWithTax);
-            Assert.Equal(26.54m, item4.PlacedPrice);
-            Assert.Equal(53.08m, item4.ExtendedPrice);
-            Assert.Equal(8.8256m, item4.DiscountAmountWithTax);
-            Assert.Equal(15.76m, item4.DiscountTotal);
-            Assert.Equal(0.112m, item4.FeeWithTax);
-            Assert.Equal(29.7248m, item4.PlacedPriceWithTax);
-            Assert.Equal(59.4496m, item4.ExtendedPriceWithTax);
-            Assert.Equal(17.6512m, item4.DiscountTotalWithTax);
-            Assert.Equal(6.3816m, item4.TaxTotal);
-
             Assert.Equal(5.6m, shipment.DiscountAmountWithTax);
             Assert.Equal(24.64m, shipment.PriceWithTax);
             Assert.Equal(0.0m, shipment.FeeWithTax);
@@ -127,21 +75,23 @@ namespace VirtoCommerce.CartModule.Test
             Assert.Equal(11.2m, payment.DiscountAmountWithTax);
             Assert.Equal(4.1424m, payment.TaxTotal);
 
-            Assert.Equal(1428.32m, cart.SubTotal);
-            Assert.Equal(177.23m, cart.SubTotalDiscount);
-            Assert.Equal(198.50m, cart.SubTotalDiscountWithTax);
-            Assert.Equal(1599.72m, cart.SubTotalWithTax);
+            Assert.Equal(1359.48m, cart.SubTotal);
+            Assert.Equal(161.47m, cart.SubTotalDiscount);
+            Assert.Equal(180.85m, cart.SubTotalDiscountWithTax);
+            Assert.Equal(1522.62m, cart.SubTotalWithTax);
+            Assert.Equal(17.00m, cart.ShippingTotal);
+            Assert.Equal(19.04m, cart.ShippingTotalWithTax);
             Assert.Equal(22.00m, cart.ShippingSubTotal);
             Assert.Equal(24.64m, cart.ShippingSubTotalWithTax);
             Assert.Equal(44.52m, cart.PaymentSubTotal);
             Assert.Equal(49.86m, cart.PaymentSubTotalWithTax);
-            Assert.Equal(156.39m, cart.TaxTotal);
-            Assert.Equal(192.23m, cart.DiscountTotal);
-            Assert.Equal(215.30m, cart.DiscountTotalWithTax);
-            Assert.Equal(13.74m, cart.FeeTotal);
-            Assert.Equal(15.39m, cart.FeeTotalWithTax);
+            Assert.Equal(150.01m, cart.TaxTotal);
+            Assert.Equal(176.47m, cart.DiscountTotal);
+            Assert.Equal(197.65m, cart.DiscountTotalWithTax);
+            Assert.Equal(13.64m, cart.FeeTotal);
+            Assert.Equal(15.28m, cart.FeeTotalWithTax);
             Assert.Equal(14.68m, cart.FeeWithTax);
-            Assert.Equal(1472.74m, cart.Total);
+            Assert.Equal(1413.18m, cart.Total);
         }
 
         [Fact]

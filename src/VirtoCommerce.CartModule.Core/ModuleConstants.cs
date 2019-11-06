8a4bcbacc1546d16e7810ca7a704499bb341913d
Andrei Kolchanov <akak1977@gmail.com>
2019-11-06 17:05:27 +0500

    Settings for abandoned cart

--- src/VirtoCommerce.CartModule.Core/ModuleConstants.cs
+++ src/VirtoCommerce.CartModule.Core/ModuleConstants.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using VirtoCommerce.Platform.Core.Settings;
+
 namespace VirtoCommerce.CartModule.Core
 {
     public static class ModuleConstants
@@ -14,6 +17,48 @@ namespace VirtoCommerce.CartModule.Core
 
                 public static string[] AllPermissions = new[] { Read, Create, Access, Update, Delete };
             }
+        }
+
+        public static class Settings
+        {
+            public static class General
+            {
+                public static readonly SettingDescriptor AbandonedCart1stEvent = new SettingDescriptor
+                {
+                    Name = "Cart.AbandonedCart1stEvent",
+                    GroupName = "Cart|General",
+                    ValueType = SettingValueType.Integer,
+                    DefaultValue = 10
+                };
+
+                public static readonly SettingDescriptor AbandonedCart2ndEvent = new SettingDescriptor
+                {
+                    Name = "Cart.AbandonedCart2ndEvent",
+                    GroupName = "Cart|General",
+                    ValueType = SettingValueType.Integer,
+                    DefaultValue = 20
+                };
+
+                public static readonly SettingDescriptor AbandonedCartDrop = new SettingDescriptor
+                {
+                    Name = "Cart.AbandonedCartDrop",
+                    GroupName = "Cart|General",
+                    ValueType = SettingValueType.Integer,
+                    DefaultValue = 30
+                };
+
+                public static IEnumerable<SettingDescriptor> AllSettings
+                {
+                    get
+                    {
+                        yield return AbandonedCart1stEvent;
+                        yield return AbandonedCart2ndEvent;
+                        yield return AbandonedCartDrop;
+                    }
+                }
+            }
+
+            public static IEnumerable<SettingDescriptor> AllSettings => General.AllSettings;
         }
     }
 }

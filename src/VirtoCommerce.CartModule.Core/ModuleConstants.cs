f933caec9ba9538cf5b2eca5824b02be3bb7cf2a
BelyakovSergey <Belyakov.Sergey@virtoway.com>
2021-09-15 09:19:44 +0300

    PT-1950: Exclude constants from code coverage

--- src/VirtoCommerce.CartModule.Core/ModuleConstants.cs
+++ src/VirtoCommerce.CartModule.Core/ModuleConstants.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.CartModule.Core
 {
+    [ExcludeFromCodeCoverage]
     public static class ModuleConstants
     {
         public static class Security

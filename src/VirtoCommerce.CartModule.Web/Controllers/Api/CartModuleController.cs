eceffe981835a0d0613c4bb7cf434ff3ba9f5711
Andrei Kolchanov <akak1977@gmail.com>
2021-10-19 14:15:39 +0500

    fix: Remove smells

--- src/VirtoCommerce.CartModule.Web/Controllers/Api/CartModuleController.cs
+++ src/VirtoCommerce.CartModule.Web/Controllers/Api/CartModuleController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hangfire;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;

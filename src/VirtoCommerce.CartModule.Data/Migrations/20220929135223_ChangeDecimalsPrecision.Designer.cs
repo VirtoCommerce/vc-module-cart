d2b4f549db2c7bdebab0b8c4bd4a478cf5230a64
Andrei Kolchanov <akak1977@users.noreply.github.com>
2022-10-04 15:38:46 +0500

    fix: Fix scale for decimals (#101)

--- src/VirtoCommerce.CartModule.Data/Migrations/20220929135223_ChangeDecimalsPrecision.Designer.cs
+++ src/VirtoCommerce.CartModule.Data/Migrations/20220929135223_ChangeDecimalsPrecision.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.CartModule.Data.Repositories;
 
@@ -11,9 +12,10 @@ using VirtoCommerce.CartModule.Data.Repositories;
 namespace VirtoCommerce.CartModule.Data.Migrations
 {
     [DbContext(typeof(CartDbContext))]
-    partial class CartDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220929135223_ChangeDecimalsPrecision")]
+    partial class ChangeDecimalsPrecision
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

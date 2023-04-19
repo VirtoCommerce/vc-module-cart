c921a89c2feac505ba454a1b23c06799e81ed6f4
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2023-04-19 09:56:48 +0300

    Fix MySql migration

--- src/VirtoCommerce.CartModule.Data.MySql/Migrations/20230413103904_AddShipmentAndPaymentComment.cs
+++ src/VirtoCommerce.CartModule.Data.MySql/Migrations/20230413103904_AddShipmentAndPaymentComment.cs
@@ -25,18 +25,6 @@ namespace VirtoCommerce.CartModule.Data.MySql.Migrations
                 maxLength: 2048,
                 nullable: true)
                 .Annotation("MySql:CharSet", "utf8mb4");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "RowVersion",
-                table: "Cart",
-                type: "timestamp(6)",
-                rowVersion: true,
-                nullable: true,
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp(6)",
-                oldRowVersion: true,
-                oldNullable: true)
-                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -48,18 +36,6 @@ namespace VirtoCommerce.CartModule.Data.MySql.Migrations
             migrationBuilder.DropColumn(
                 name: "Comment",
                 table: "CartPayment");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "RowVersion",
-                table: "Cart",
-                type: "timestamp(6)",
-                rowVersion: true,
-                nullable: true,
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp(6)",
-                oldRowVersion: true,
-                oldNullable: true)
-                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
         }
     }
 }

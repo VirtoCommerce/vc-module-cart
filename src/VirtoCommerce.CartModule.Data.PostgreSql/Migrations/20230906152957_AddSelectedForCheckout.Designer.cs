a23bccc2c8dbe1a16039f32ee963efba87c3dd04
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-09-06 17:33:07 +0200

    feat: add MySql & Postgre migrations

--- src/VirtoCommerce.CartModule.Data.PostgreSql/Migrations/20230906152957_AddSelectedForCheckout.Designer.cs
+++ src/VirtoCommerce.CartModule.Data.PostgreSql/Migrations/20230906152957_AddSelectedForCheckout.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VirtoCommerce.CartModule.Data.Repositories;
@@ -11,9 +12,10 @@ using VirtoCommerce.CartModule.Data.Repositories;
 namespace VirtoCommerce.CartModule.Data.PostgreSql.Migrations
 {
     [DbContext(typeof(CartDbContext))]
-    partial class CartDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230906152957_AddSelectedForCheckout")]
+    partial class AddSelectedForCheckout
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/VirtoCommerce.CartModule.Data.PostgreSql/Migrations/20230906152957_AddSelectedForCheckout.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.CartModule.Data.PostgreSql.Migrations
+{
+    public partial class AddSelectedForCheckout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SelectedForCheckout",
+                table: "CartLineItem",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelectedForCheckout",
+                table: "CartLineItem");
+        }
+    }
+}

2e10b7f12613129a32a4e4759eaf852bbe2c9ed1
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-03-19 08:53:51 +0100

    feat: add find product wishlist by prodcuts

--- src/VirtoCommerce.CartModule.Data/Services/WishlistService.cs
+++ src/VirtoCommerce.CartModule.Data/Services/WishlistService.cs
@@ -31,16 +31,66 @@ namespace VirtoCommerce.CartModule.Data.Services
             var cacheKeyPrefix = CacheKey.With(GetType(), nameof(FindProductsInWishlistsAsync), customerId, storeId);
 
             var models = await _platformMemoryCache.GetOrLoadByIdsAsync(cacheKeyPrefix, productIds,
-                missingIds => GetByIdsNoCache(customerId, organizationId, storeId, missingIds),
+                missingIds => FindProductsInWishlistsNoCacheAsync(customerId, organizationId, storeId, missingIds),
                 ConfigureCache);
 
             return models.Where(x => x.InWishlist).Select(x => x.Id).ToList();
         }
 
-        protected virtual async Task<IList<InternalEntity>> GetByIdsNoCache(string customerId, string organizationId, string storeId, IList<string> productIds)
+        public virtual async Task<IDictionary<string, IList<string>>> FindWishlistsByProductsAsync(string customerId, string organizationId, string storeId, IList<string> productIds)
+        {
+            var cacheKeyPrefix = CacheKey.With(GetType(), nameof(FindWishlistsByProductsAsync), customerId, storeId);
+
+            var models = await _platformMemoryCache.GetOrLoadByIdsAsync(cacheKeyPrefix, productIds,
+                 missingIds => FindWishlistsByProductsNoCacheAsync(customerId, organizationId, storeId, missingIds),
+                 ConfigureCache);
+
+            return models.ToDictionary(x => x.Id, x => x.WishlistIds);
+        }
+
+        protected virtual async Task<IList<InternalEntity>> FindProductsInWishlistsNoCacheAsync(string customerId, string organizationId, string storeId, IList<string> productIds)
         {
             using var repository = _repositoryFactory();
 
+            var query = GetCustomerWishListsQuery(repository, customerId, organizationId, storeId);
+
+            var result = await query
+                .SelectMany(cart => cart.Items)
+                .Where(lineItem => productIds.Contains(lineItem.ProductId))
+                .Select(lineItem => lineItem.ProductId)
+                .Distinct()
+                .Select(x => new InternalEntity { Id = x, CustomerId = customerId, InWishlist = true })
+                .ToListAsync();
+
+            result.AddRange(productIds.Except(result.Select(x => x.Id))
+                .Select(x => new InternalEntity { Id = x, CustomerId = customerId }));
+
+            return result;
+        }
+
+        protected virtual async Task<IList<InternalEntity>> FindWishlistsByProductsNoCacheAsync(string customerId, string organizationId, string storeId, IList<string> productIds)
+        {
+            using var repository = _repositoryFactory();
+
+            var query = GetCustomerWishListsQuery(repository, customerId, organizationId, storeId);
+
+            var result = await query
+                .SelectMany(cart => cart.Items)
+                .Where(lineItem => productIds.Contains(lineItem.ProductId))
+                .GroupBy(lineItem => lineItem.ProductId)
+                .Select(lineItemGroup => new InternalEntity
+                {
+                    Id = lineItemGroup.Key,
+                    WishlistIds = lineItemGroup.Select(x => x.ShoppingCartId).ToList(),
+                    CustomerId = customerId,
+                })
+                .ToListAsync();
+
+            return result;
+        }
+
+        protected virtual IQueryable<ShoppingCartEntity> GetCustomerWishListsQuery(ICartRepository repository, string customerId, string organizationId, string storeId)
+        {
             var query = repository.ShoppingCarts.Where(cart =>
                                cart.StoreId == storeId &&
                                cart.Type == ModuleConstants.WishlistCartType &&
@@ -60,18 +110,7 @@ namespace VirtoCommerce.CartModule.Data.Services
 
             query = query.Where(predicate);
 
-            var result = await query
-                .SelectMany(cart => cart.Items)
-                .Where(lineItem => productIds.Contains(lineItem.ProductId))
-                .Select(lineItem => lineItem.ProductId)
-                .Distinct()
-                .Select(x => new InternalEntity { Id = x, CustomerId = customerId, InWishlist = true })
-                .ToListAsync();
-
-            result.AddRange(productIds.Except(result.Select(x => x.Id))
-                .Select(x => new InternalEntity { Id = x, CustomerId = customerId }));
-
-            return result;
+            return query;
         }
 
         protected virtual void ConfigureCache(MemoryCacheEntryOptions cacheOptions, string id, InternalEntity model)
@@ -83,6 +122,7 @@ namespace VirtoCommerce.CartModule.Data.Services
         {
             public string CustomerId { get; set; }
             public bool InWishlist { get; set; }
+            public IList<string> WishlistIds { get; set; }
         }
     }
 }

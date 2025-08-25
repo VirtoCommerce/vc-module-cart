44d3dd5708eda38a60a21fdfdc8491ce1e738cec
yuskithedeveloper <yuskianywhere@gmail.com>
2025-08-25 16:58:25 -0500

    Sharing modes

--- src/VirtoCommerce.CartModule.Core/Model/CartSharingModes.cs
+++ src/VirtoCommerce.CartModule.Core/Model/CartSharingModes.cs
@@ -2,7 +2,8 @@ namespace VirtoCommerce.CartModule.Core.Model;
 
 public static class CartSharingModes
 {
-    public const string Anyone = "Anyone";
+    public const string Private = "Private";
+    public const string Anyone = "Anyone";//TODO: AnyoneAnonymous & AnyoneAuthorized ?
     public const string Organization = "Organization";
     public const string User = "User";
 }

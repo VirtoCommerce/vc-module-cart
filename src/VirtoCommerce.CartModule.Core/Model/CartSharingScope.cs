7892364e3c67f0db4c486e05960ca8fe1dea184c
yuskithedeveloper <yuskianywhere@gmail.com>
2025-08-26 16:12:11 -0500

    Sharing scopes

--- src/VirtoCommerce.CartModule.Core/Model/CartSharingScope.cs
+++ src/VirtoCommerce.CartModule.Core/Model/CartSharingScope.cs
@@ -3,7 +3,8 @@ namespace VirtoCommerce.CartModule.Core.Model;
 public static class CartSharingScope
 {
     public const string Private = "Private";
-    public const string Anyone = "Anyone";//TODO: AnyoneAnonymous & AnyoneAuthorized ?
+    public const string AnyoneAnonymous = "AnyoneAnonymous";
+    public const string AnyoneAuthorized = "AnyoneAuthorized";
     public const string Organization = "Organization";
     public const string User = "User";
 }
